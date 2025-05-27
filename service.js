109044bbdf8705903e20ca9daa181c72a0400d28
Cayro Shop <cayroshop.db@gmail.com>
2025-05-27 19:46:05 +0530

    resolved bug

--- service.js
+++ service.js
@@ -134,7 +134,7 @@
 //service.js
 import { STUDENT_LATEST } from "./newStudentModel.model.js";
 import { School } from "./school.js";
-import { KINDERGARTEN_STUDENT } from "./kindergartenStudent.model.js";
+import { KINDERGARTEN_STUDENT } from "./kindergarten.model.js";
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
 
