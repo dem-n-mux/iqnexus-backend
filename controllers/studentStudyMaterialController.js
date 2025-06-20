2a070a79d8860fa6f39d3d7b9469bdd6f3df6a1d
AJ172003 <sumitsrawat2003@gmail.com>
2025-06-21 00:54:02 +0530

    made changes to study-material

--- controllers/studentStudyMaterialController.js
+++ controllers/studentStudyMaterialController.js
@@ -1,15 +1,18 @@
-import AWS from 'aws-sdk';
-import fs from 'fs';
+import AWS from "aws-sdk";
+import fs from "fs";
+import { StudyMaterial } from "../services/studyMaterialService.js";
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_key ,
+  accessKeyId: process.env.AWS_KEY,
   secretAccessKey: process.env.AWS_SECRET,
   region: process.env.AWS_REGION,
 });
 const s3 = new AWS.S3();
 export const addStudentStudyMaterial = async (req, res) => {
-
-const { name, age ,className, subject ,fee   } = req.body;
+  console.log("AWS Key:", process.env.AWS_KEY);
+  console.log("AWS Secret:", process.env.AWS_SECRET);
+  console.log("AWS Region:", process.env.AWS_REGION);
+  const { name, age, className, subject, fee } = req.body;
 
   if (!req.file) return res.status(400).json({ error: "No file uploaded" });
 
@@ -20,22 +23,22 @@ const { name, age ,className, subject ,fee   } = req.body;
     Key: `pdfs/${Date.now()}_${req.file.originalname}`,
     Body: fileContent,
     ContentType: "application/pdf",
-    ACL: "public-read",
   };
 
   try {
     const result = await s3.upload(params).promise();
     fs.unlinkSync(req.file.path); // optional: cleanup temp file
-    const resultMongo = await STUDENT_LATEST.create({
-        category: name,
-        class: className,
-        examId: subject,
-        cost: fee,
-        pdfLink : result.Location,
+    const resultMongo = await StudyMaterial.create({
+      category: name,
+      class: className,
+      examId: subject,
+      cost: fee,
+      pdfLink: result.Location,
+    });
+    resultMongo.save();
+    console.log("File uploaded successfully. Location:", result.Location);
 
-});
-
-console.log(result.Location)
+    console.log(result.Location);
     res.json({
       message: "Upload successful",
       url: result.Location,
@@ -46,5 +49,4 @@ console.log(result.Location)
     console.error("S3 Upload Error:", err);
     res.status(500).json({ error: "Upload failed" });
   }
-}
-
+};
